37c25e77b967aaea211a89d696c21cbf8bfe8c01
yurii <urakarabin123@gmail.com>
2022-07-20 01:12:24 +0300

    add button to hide text

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const btnColor = document.querySelector('#btn-color');
 const text = document.querySelector('.text');
-
+const btnHide = document.querySelector('#btn-hide');
 btnColor.addEventListener('click', ()=>{
     if(text.classList.contains("color")){
         text.classList.remove("color");
     }else{
         text.classList.add("color");
+    } 
+})
+
+btnHide.addEventListener('click', ()=>{
+    if(text.classList.contains("hide")){
+        text.classList.remove("hide");
+    }else{
+        text.classList.add("hide");
     } 
 })
